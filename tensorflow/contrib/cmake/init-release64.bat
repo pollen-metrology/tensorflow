1c4fffd2d35ab4444c23bb8671c1e30401455781
Aurélien Labrosse <aurelien@pollen-metrology.com>
2017-11-29 11:56:44 +0100

    Add bootstrap script for Windows x86_64 build Debug/Release

---
+++ tensorflow/contrib/cmake/init-release64.bat
@@ -0,0 +1,13 @@
+c:\dev\tools\cmake-3.7.2\bin\cmake .. ^
+-G"Visual Studio 14 2015 Win64" ^
+-DCMAKE_GENERATOR="Visual Studio 14 2015 Win64" ^
+-DCMAKE_BUILD_TYPE=Release ^
+-Dtensorflow_BUILD_SHARED_LIB=TRUE ^
+-Dtensorflow_BUILD_PYTHON_BINDINGS=OFF ^
+-DBUILD_SHARED_LIBS=false ^
+-Dtensorflow_ENABLE_GRPC_SUPPORT=OFF ^
+-Dtensorflow_BUILD_CC_TESTS=OFF ^
+-Dtensorflow_OPTIMIZE_FOR_NATIVE_ARCH=true ^
+-Dtensorflow_WIN_CPU_SIMD_OPTIONS=/arch:AVX2 ^
+-DEigen3_DIR=c:\dev\dist\msvc14\amd64\eigen\release\share\eigen3\cmake\ ^
+-DCMAKE_INSTALL_PREFIX=C:\dev\dist\msvc14\amd64\tensorflow\release
